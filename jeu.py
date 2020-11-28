e4368593c76870ca09e0d7732bc33e33a537dbca
raphael <raphael.gerbeaux@gmail.com>
2020-11-28 13:33:27 +0100

    teat 03 mouv ok

--- jeu.py
+++ jeu.py
@@ -32,23 +32,38 @@ width = ecran.get_width()
 raph_h, raph_w = raph.get_size()
 """raph.x = int(width / 2 - raph_w / 2)
 raph.y = int(height / 2 - raph_h / 2)"""
-BLUE = (0, 100, 200)
+BLUE = (0, 100, 250)
 BLACK = (0, 0, 0)
-mur = pygame.Surface((30, 30))
-mur.fill(BLACK)
-"""raph.prt()
-ecran.fill(GREY)"""
-continuer = True
-"""raph.x = int(30)
-raph.y = int(30)"""
+mur = pygame.image.load("assets/mur.jpg").convert_alpha()
+mur = pygame.transform.scale(mur, (30, 30))
 
+fond = pygame.image.load("assets/fond.png").convert_alpha()
+fond = pygame.transform.scale(fond, (30, 30))
+
+
+nenu = pygame.image.load("assets/nenuphar.png").convert_alpha()
+nenu = pygame.transform.scale(nenu, (30, 30))
+
+
+continuer = True
+
+
+
+raph.x = 30
+raph.y = 30
 
 def draw(ecran, niveau):
     for j, ligne in enumerate(niveau):
         for i, case in enumerate(ligne):
             if case == 1:
                 ecran.blit(mur, (i * 30, j * 30))
-
+            elif case == 0:
+                ecran.blit(fond,(i * 30, j * 30))
+            elif case == 3:
+                ecran.blit(nenu, (i * 30, j * 30))
+            elif case == 2:
+                ecran.blit(fond, (i * 30, j * 30))
+                ecran.blit(raph.image, (raph.x, raph.y))
 
 def one(x, y):
     print(x, y)
@@ -58,11 +73,13 @@ def one(x, y):
     elif niveau[y][x] == 0:
         return True
     elif niveau[y][x] == 2:
-        pass
+        return True
+    elif niveau[y][x] == 3:
+        return True
 
 
-raph.x = 30
-raph.y = 30
+
+
 
 while continuer:
     for event in pygame.event.get():
@@ -73,34 +90,32 @@ while continuer:
             y = int(raph.y // 30)
             one(x, y)
             if event.key == pygame.K_RIGHT:
-                print("right")
-                if one(x + 1, y):
+                print("right ",raph.x)
+                if  raph.x <= 390 and one(x + 1, y):
                     raph.x += 30
                 else:
                     raph.x = raph.x
             if event.key == pygame.K_LEFT:
                 print("left")
-                if one(x - 1, y):
+                if one(x - 1, y) and raph.x > 0:
                     raph.x -= 30
                 else:
                     raph.x = raph.x
             if event.key == pygame.K_UP:
                 print("up")
-                if one(x, y - 1):
+                if one(x, y - 1) and raph.y > 0:
                     raph.y -= 30
                 else:
                     raph.y = raph.y
             if event.key == pygame.K_DOWN:
                 print("down")
-                if one(x, y + 1):
+                if raph.y <= 390 and one(x, y + 1):
                     raph.y += 30
                 else:
                     raph.y = raph.y
                 """x, y = raph.get_pos()
                 print("X =", x, "Y =", y)"""
-            """if event.type == pygame.QUIT:
-                continuer = False
-                sys.exit()"""
+
     ecran.fill(BLUE)
     draw(ecran, niveau)
     raph.prt()
