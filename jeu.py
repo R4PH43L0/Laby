3ea70504f7157d226370fb01e703552366a1241e
raphael <raphael.gerbeaux@gmail.com>
2020-12-30 11:23:21 +0100

    def draw

--- jeu.py
+++ jeu.py
@@ -80,20 +80,19 @@ raph.y = 30
 def draw(ecran, niveau):
     """Parcour de la liste niveau puis, blit des differents éléments."""
 
-
-for j, ligne in enumerate(niveau):
-    for i, case in enumerate(ligne):
-        if case == 1:
-            ecran.blit(mur, (i * 30, j * 30))
-        elif case == 0:
-            ecran.blit(fond, (i * 30, j * 30))
-        elif case == 3:
-            ecran.blit(nenu, (i * 30, j * 30))
-        elif case == 2:
-            ecran.blit(raph.image, (raph.x, raph.y))
-            ecran.blit(fond, (i * 30, j * 30))
-        elif case == 4:
-            ecran.blit(chat, (i * 30, j * 30))
+    for j, ligne in enumerate(niveau):
+        for i, case in enumerate(ligne):
+            if case == 1:
+                ecran.blit(mur, (i * 30, j * 30))
+            elif case == 0:
+                ecran.blit(fond, (i * 30, j * 30))
+            elif case == 3:
+                ecran.blit(nenu, (i * 30, j * 30))
+            elif case == 2:
+                ecran.blit(raph.image, (raph.x, raph.y))
+                ecran.blit(fond, (i * 30, j * 30))
+            elif case == 4:
+                ecran.blit(chat, (i * 30, j * 30))
 
 
 def mouv(x, y):
