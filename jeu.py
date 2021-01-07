f8bc4c0ab3a92719c0ccf0aa4bcd563d073fabe1
raphael <raphael.gerbeaux@gmail.com>
2021-01-07 11:45:09 +0100

    class Perso in perso.py

--- jeu.py
+++ jeu.py
@@ -5,31 +5,8 @@
 from os import getlogin
 import pygame as pg
 from tableaux import niveau
+from modeles.perso import Perso
 
-
-class Perso(object):
-    """Definition of the item."""
-
-    def __init__(self, x=0, y=0):
-        """Definition of the position of the item with x and y."""
-        self.x = x
-        self.y = y
-
-        self.image = pg.image.load("assets/crapeaux.png").convert_alpha()
-
-        self.image = pg.transform.scale(self.image, (30, 30))
-
-    def imprime_perso(self):
-        """Blit function of pygame to print the item."""
-        ecran.blit(self.image, (self.x, self.y))
-
-    def get_size(self):
-        """Return the size of the item."""
-        return self.image.get_size()
-
-    def get_pos(self):
-        """Return the position of the item."""
-        return (self.x, self.y)
 
 
 NAME = getlogin()
